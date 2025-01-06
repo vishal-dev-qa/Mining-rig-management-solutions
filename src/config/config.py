6be61ac5a2d2d17412290ef29862dce27e9a9593
vishal-dev-qa <vishal@quantumalchemy.ca>
2025-01-06 10:09:01 +0000

    added config for cgminer

---
+++ src/config/config.py
@@ -0,0 +1,19 @@
+# Port of cgminer
+CG_PORT = 4028
+
+# commands we can use to get info from cgminer
+SUMMARY_COMMAND = 'summary'
+STATS_COMMAND = 'stats'
+CONFIG_COMMAND = 'config'
+RESTART_COMMAND = 'restart'
+PING_COMMAND = 'ping'
+POOLS_COMMAND = 'pools'
+ENABLE_COMMAND = 'enable'
+DISABLE_COMMAND = 'disable'
+SAVE_COMMAND = 'save'
+LOAD_COMMAND = 'load'
+VOLTAGE_COMMAND = 'voltage'
+TEMPERATURE_COMMAND = 'temperature'
+FAN_COMMAND = 'fans'
+QUIT_COMMAND = 'quit'
+HELP_COMMAND = 'help'
